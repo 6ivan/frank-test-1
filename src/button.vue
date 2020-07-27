77213ac7af5ed2d4126205ac7f5109e9a8f2f32c
6ivan <619183041@qq.com>
2020-07-27 17:29:24 +0800

    vue单组件

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class='g-button'>按钮</button>
+</template>
+<script>
+    export default {
+        name
+    }
+</script>
+<style lang="scss">
+    .g-button {
+        font-size: var(--fons-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--button-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg)
+    }
+
+    .g-button:hover {
+        border-color: var(--boder-color-hover);
+    }
+
+    .g-button:active {
+        background-color: var(--button-active-bg);
+    }
+
+    .g-button:focus {
+        outline: none;
+    }
+
+</style>
